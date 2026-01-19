8d83d0061d91d11358348450aa1827e4b00d7042
Luis David Garcia <hookeri_bartz@aleeas.com>
2026-01-19 02:28:04 -0800

    Formatted ubx

--- Core/Inc/ubx/ubx.hpp
+++ Core/Inc/ubx/ubx.hpp
@@ -42,63 +42,75 @@
 static constexpr int MAX_MESSAGE_LENGTH = 1000;
 static constexpr int MAX_PAYLOAD_LENGTH = 92;
 
-class UBX {
+class UBX
+{
 public:
-	//********* SYNC CHAR SECTION **********
-	static constexpr uint8_t sync_char_1 = 0xB5;
-	static constexpr uint8_t sync_char_2 = 0x62;
+    //********* SYNC CHAR SECTION **********
+    static constexpr uint8_t sync_char_1 = 0xB5;
+    static constexpr uint8_t sync_char_2 = 0x62;
 
-	//********* NAV MESSAGE SECTION **********
-	static constexpr uint8_t nav_pvt = 0x07;
+    //********* NAV MESSAGE SECTION **********
+    static constexpr uint8_t nav_pvt = 0x07;
 
-	//********* ACK MESSAGE SECTION **********
-	static constexpr uint8_t ack_ack = 0x01;
-	static constexpr uint8_t ack_nak = 0x00;
+    //********* ACK MESSAGE SECTION **********
+    static constexpr uint8_t ack_ack = 0x01;
+    static constexpr uint8_t ack_nak = 0x00;
 
-	//********* CFG MESSAGE SECTION **********
-	static constexpr uint8_t cfg_prt = 0x00;
-	static constexpr uint8_t cfg_msg = 0x01;
-	static constexpr uint8_t cfg_inf = 0x02;
-	static constexpr uint8_t cfg_rate = 0x08;
+    //********* CFG MESSAGE SECTION **********
+    static constexpr uint8_t cfg_prt = 0x00;
+    static constexpr uint8_t cfg_msg = 0x01;
+    static constexpr uint8_t cfg_inf = 0x02;
+    static constexpr uint8_t cfg_rate = 0x08;
 
-	enum class MsgClass : uint8_t {
-		nav = 0x01,
-		rxm = 0x02,
-		inf = 0x04,
-		ack = 0x05,
-		cfg = 0x06,
-		upd = 0x09,
-		mon = 0x0A,
-		aid = 0x0B,
-		tim = 0x0D,
-		esf = 0x10,
-		mga = 0x13,
-		log = 0x21,
-		sec = 0x27,
-		hnr = 0x28,
-	};
+    enum class MsgClass : uint8_t
+    {
+        nav = 0x01,
+        rxm = 0x02,
+        inf = 0x04,
+        ack = 0x05,
+        cfg = 0x06,
+        upd = 0x09,
+        mon = 0x0A,
+        aid = 0x0B,
+        tim = 0x0D,
+        esf = 0x10,
+        mga = 0x13,
+        log = 0x21,
+        sec = 0x27,
+        hnr = 0x28,
+    };
 
-	enum class FixFlags : uint8_t {
-		no_fix = 0,
-		dead_reckoning_only = 1,
-		two_d_fix = 2,
-		three_d_fix = 3,
-		gnss_dead_reckoning_combined = 4,
-		time_only_fix = 5,
-	};
+    enum class FixFlags : uint8_t
+    {
+        no_fix = 0,
+        dead_reckoning_only = 1,
+        two_d_fix = 2,
+        three_d_fix = 3,
+        gnss_dead_reckoning_combined = 4,
+        time_only_fix = 5,
+    };
 
-	struct UbxMessage {
-		uint8_t sync1{sync_char_1};
-		uint8_t sync2{sync_char_2};
-		MsgClass msgClass{MsgClass::nav};
-		uint8_t msgID{cfg_prt};
-		uint16_t payloadLength{0};
-		std::vector<uint8_t> payload{};
-		uint8_t checksumA{0};
-		uint8_t checksumB{0};
-	};
+    struct UbxMessage
+    {
+        uint8_t sync1
+            { sync_char_1 };
+        uint8_t sync2
+            { sync_char_2 };
+        MsgClass msgClass
+            { MsgClass::nav };
+        uint8_t msgID
+            { cfg_prt };
+        uint16_t payloadLength
+            { 0 };
+        std::vector<uint8_t> payload
+            { };
+        uint8_t checksumA
+            { 0 };
+        uint8_t checksumB
+            { 0 };
+    };
 
-	void ComputeChecksum(UbxMessage& message);
+    void ComputeChecksum(UbxMessage &message);
 };
 
 #endif // UBX_HPP
--- Core/Src/ubx/ubx.cpp
+++ Core/Src/ubx/ubx.cpp
@@ -4,23 +4,23 @@
  * @brief   Calculate the checksum values (checksumA and checksumB) for a UBX message
  * @param 	Message to calculate the checksum for
  */
-void UBX::ComputeChecksum(UbxMessage& message)
+void UBX::ComputeChecksum(UbxMessage &message)
 {
-	message.checksumA = static_cast<uint8_t>(message.msgClass);
-	message.checksumB = message.checksumA;
+    message.checksumA = static_cast<uint8_t>(message.msgClass);
+    message.checksumB = message.checksumA;
 
-	message.checksumA += message.msgID;
-	message.checksumB += message.checksumA;
+    message.checksumA += message.msgID;
+    message.checksumB += message.checksumA;
 
-	message.checksumA += message.payloadLength % (1 << 8);
-	message.checksumB += message.checksumA;
+    message.checksumA += message.payloadLength % (1 << 8);
+    message.checksumB += message.checksumA;
 
-	message.checksumA += message.payloadLength >> 8;
-	message.checksumB += message.checksumA;
+    message.checksumA += message.payloadLength >> 8;
+    message.checksumB += message.checksumA;
 
-	for (int i = 0; i < message.payloadLength; i++) {
-		message.checksumA += message.payload.at(i);
-		message.checksumB += message.checksumA;
-	}
+    for (int i = 0; i < message.payloadLength; i++) {
+        message.checksumA += message.payload.at(i);
+        message.checksumB += message.checksumA;
+    }
 }
 

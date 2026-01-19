c554c6e042a32addf329999a1a83e805121ae630
Luis David Garcia <hookeri_bartz@aleeas.com>
2026-01-19 02:27:11 -0800

    Formatted app

--- Core/Inc/app.hpp
+++ Core/Inc/app.hpp
@@ -10,17 +10,16 @@
 #include "stm32l4xx_hal.h"
 
 // Cpp function to call into main event loop
-void app_main_CPP(I2C_HandleTypeDef* hi2c1, UART_HandleTypeDef* huart4);
+void app_main_CPP(I2C_HandleTypeDef *hi2c1, UART_HandleTypeDef *huart4);
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-	// C function to call into Cpp event loop from main
-	void app_main_C(I2C_HandleTypeDef* hi2c1, UART_HandleTypeDef* huart4);
+    // C function to call into Cpp event loop from main
+    void app_main_C(I2C_HandleTypeDef *hi2c1, UART_HandleTypeDef *huart4);
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif /* APP_HPP */
--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -5,24 +5,30 @@
  */
 
 #include <mpu9250_imu/mpu9250_imu.hpp>
+#include <gps/gps.hpp>
 #include "app.hpp"
 
 // All C++ Includes must go here for C not to freak out
 
 // Main Cpp event loop to run application
-void app_main_CPP(I2C_HandleTypeDef* hi2c1, UART_HandleTypeDef* huart4) {
-	IMU imu{hi2c1};
+void app_main_CPP(I2C_HandleTypeDef *hi2c1, UART_HandleTypeDef *huart4)
+{
+    IMU imu
+        { hi2c1 };
 
-	while (1)
-	{
-		imu.Read();
-	}
+    GPS gps
+        { 2026, huart4 };
+
+    while (1) {
+        imu.Read();
+    }
 }
 
 // Define all C function calls from main.c below
 extern "C"
 {
-	void app_main_C(I2C_HandleTypeDef* hi2c1, UART_HandleTypeDef* huart4) {
-		app_main_CPP(hi2c1, huart4);
-	}
+    void app_main_C(I2C_HandleTypeDef *hi2c1, UART_HandleTypeDef *huart4)
+    {
+        app_main_CPP(hi2c1, huart4);
+    }
 }
